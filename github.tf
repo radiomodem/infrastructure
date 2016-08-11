a705ce0478e57124124450a37e51ebefba985505
Kasper Isager <kasperisager@gmail.com>
2016-08-11 22:08:12 +0200

    Commit GitHub Pages resources

---
+++ github.tf
@@ -0,0 +1,33 @@
+variable "github_pages_ips" {
+  default = {
+    "0" = "192.30.252.153"
+    "1" = "192.30.252.154"
+  }
+}
+
+resource "cloudflare_record" "radiomodem" {
+  count = "2"
+  domain = "radiomodem.dk"
+  name = "radiomodem.dk"
+  value = "${lookup(var.github_pages_ips, count.index)}"
+  type = "A"
+  proxied = true
+}
+
+resource "cloudflare_record" "www" {
+  count = "2"
+  domain = "radiomodem.dk"
+  name = "www"
+  value = "${lookup(var.github_pages_ips, count.index)}"
+  type = "A"
+  proxied = true
+}
+
+resource "cloudflare_record" "patterns" {
+  count = "2"
+  domain = "radiomodem.dk"
+  name = "patterns"
+  value = "${lookup(var.github_pages_ips, count.index)}"
+  type = "A"
+  proxied = true
+}
